39b4e2c704ef4a99b56723cd62575c88098dc797
Jay Williams <clubugly@gmail.com>
2011-11-05 23:56:06 -0400

    Holy crap, I'm finished.
    Took me almost 6 hours to figure out why local storage was not working.
    Dang "S" added to "radio" when it shouldn't have been lol

--- js/main.js
+++ js/main.js
@@ -33,8 +33,8 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 	//find value of selected radio buttons.
 	function getSelectedRadio(){
-		var radio = document.forms[0].sex;
-		for(var i=0; i<radio.length; i++){
+		var radios = document.forms[0].sex;
+		for(var i=0; i<radios.length; i++){
 			if(radios[i].checked){
 				sexValue = radios[i].value;
 			}
@@ -49,35 +49,59 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 	}
 	
+	function toggleControls(n){
+		switch(n){
+			case "on":
+				$('beerForm').style.display = "none";
+				$('clear').style.display = "inline";
+				$('displayLink').style.display = "none";
+				$('addNew').style.display = "inline";
+				break;
+			case "off":
+				$('beerForm').style.display = "block";
+				$('clear').style.display = "inline";
+				$('displayLink').style.display = "inline";
+				$('addNew').style.display = "inline";
+				$('items').style.display = "none";				
+				break;
+			default:
+				return false;		
+		}
+	}
+	
 	function storeData(){
 		var id 			= Math.floor(Math.random()*100000001);
 		// collect all form field values and store in an object
 		// object properties contain array with the form label and input value
-		getSelectRadio();
+		getSelectedRadio();
 		getCheckboxValue();
 		var item 			= {};
 			item.fname		= ["Name:", $('fname').value];
 			item.email		= ["Email:", $('email').value];
 			item.url 		= ["Home Page:", $('url').value];
-			//item.sex		= ["Sex:", sexValue];
-			
+			item.sex		= ["Sex:", sexValue];			
 			item.borndate 	= ["Date of Birth:", $('borndate').value];
-			item.group 		= ["Group:", $('groups').value];
-			item.range 		= ["Quantity:", $('range').value];
+			item.group	 	= ["Beer Type:", $('groups').value];
+			item.quantity	= ["Quantity:", $('quantity').value];
 			item.comments 	= ["Additional Info:", $('comments').value];
-			//item.terms 		= ["TOS:", $('terms').termsValue];
+			item.terms 		= ["TOS:", $('terms').termsValue];
 			//save data into local storage: Use Stringify to convert our object to string
 		localStorage.setItem(id, JSON.stringify(item));
 		alert("Shopping Info Saved!");	
 	}
 	
 	function getData(){
+		toggleControls("on");
+		if(localStorage.length === 0){
+			alert("There is no data stored.");
+		}
 		//write data from local storage to the browser
 		var makeDiv = document.createElement('div');
 		makeDiv.setAttribute("id", "items");
 		var makeList = document.createElement('ul');
 		makeDiv.appendChild(makeList);
 		document.body.appendChild(makeDiv);
+		$('items').style.display = "block";
 		for(var i=0, len=localStorage.length; i<len;i++){
 			var makeli = document.createElement('li');
 			makeList.appendChild(makeli);
@@ -94,8 +118,19 @@ window.addEventListener("DOMContentLoaded", function(){
 				makeSubli.innerHTML = optSubText;
 			}
 		}
-	
 	}
+	
+	function clearLocal(){
+		if(localStorage.length === 0){
+			alert("There is no data to clear.")
+		}else{
+			localStorage.clear();
+			alert("Shopping order is deleted!");
+			window.location.reload();
+			return false;
+		}
+	}
+	
 	//Variable defaults
 	var beerSelection = ["--Choose your can of beer--", "Bud Light", "Bud Select", "Sam Adams Cherry", "Sam Adams October Fest", "Corona", "Corona Light", "Milwaukee's Best"],
 		sexValue,
@@ -106,12 +141,9 @@ window.addEventListener("DOMContentLoaded", function(){
 	//Set Link & Submit Click Events
 	var displayLink = $('displayLink');
 	displayLink.addEventListener("click", getData);
-	/*var clearLink = $('clear');
-	clearlink.addEventListener("click", clearLocal); */
+	var clearLink = $('clear');
+	clearLink.addEventListener("click", clearLocal);
 	var save = $('submit');
 	save.addEventListener("click", storeData);
 
 });
-
-
-
